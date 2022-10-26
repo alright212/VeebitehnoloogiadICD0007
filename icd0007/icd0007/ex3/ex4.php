99751e994e51ff6c935e7563d2668676464fe941
glkink <99361502+alright212@users.noreply.github.com>
2022-10-26 20:42:43 +0300

    Delete icd0007/icd0007 directory

--- icd0007/icd0007/ex3/ex4.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$inputList = [1, 4, 2, 0];
-
-$listAsString = join(', ', $inputList);
-$restoredList = explode(', ', $listAsString);
-$restoredList = array_map(fn($each)=> intval($each), $restoredList);
-var_dump($restoredList);
-print var_dump($restoredList===$inputList);
--- icd0007/icd0007/ex4/redirect/saver.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$data = $_POST['data'] ?? '';
-$url = 'index.php?message=' . urlencode("Data\n saved!");
-header('Location: ' . $url);
